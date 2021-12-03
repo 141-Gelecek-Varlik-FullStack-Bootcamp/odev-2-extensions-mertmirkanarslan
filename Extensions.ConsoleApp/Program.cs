082e5dfe68d67a1eeec4cebda48adbbd01145a70
Mert Mirkan Arslan <mertmirkanarslan@gmail.com>
2021-12-03 19:15:11 +0300

    First extension added
    first extension and foldering edited

---
+++ Extensions.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extensions.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime zaman = DateTime.Now;
+            Console.WriteLine("Turkish Time at GMT+3 is:");
+            Console.WriteLine(zaman.ToString());
+            Console.WriteLine("Turkish Time at GMT+2 is:");
+            Console.WriteLine(Homework_2.Core.Extensions.ToOldDateTime(zaman));
+        }
+    }
+}
---
+++ Homework-2.Core/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework_2.Core
+{
+    public static class Extensions
+    {
+        //Turkey is in the UTC+3 timezone. Let's make it UTC+2 (Central European Time) as it used to be.
+        public static string ToOldDateTime(this DateTime oldTimezone)
+        {
+            return oldTimezone.AddHours(-1).ToLongTimeString();
+        }
+    }
+}
